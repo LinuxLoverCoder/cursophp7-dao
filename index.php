eeb8c2cf9ca50d55ed97496f164d4b35069c47df
LinuxLoverCoder <brenosantanabruno@live.com>
2018-11-12 01:27:24 -0200

    Modificações no Select

--- index.php
+++ index.php
@@ -9,9 +9,34 @@ require_once("config.php");
 //echo json_encode($usuarios);
 
 
-$root = new Usuario();
-$root->loadById(1);
 
-echo $root;
+//carrega um usário
+
+//$root = new Usuario();
+//$root->loadById(1);
+
+//echo $root;
+
+//carrega uma lista de usuários
+
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//carrega uma lista de usuários buscando pelo login
+
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+
+$usuario->login("jose", "123456");
+
+echo $usuario;
+
 
  ?>
